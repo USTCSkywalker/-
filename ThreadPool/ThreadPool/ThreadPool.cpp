fd60ea7bdd90cb94efa36b811bc2c302f99e0e7a
Skywa1ker <HephaistosTech@gmail.com>
2023-06-27 12:46:01 +0800

    2 Pools

---
+++ ThreadPool/ThreadPool/ThreadPool.cpp
@@ -0,0 +1,41 @@
+﻿// ThreadPool.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+#include "ThreadPool.h"
+
+int main() {
+    ThreadPool pool(4);
+    std::vector< std::future<int> > results;
+
+    for (int i = 0; i < 8; ++i) {
+        results.emplace_back(
+            pool.enqueue([i] {
+                std::cout << "hello " << i << std::endl;
+                std::this_thread::sleep_for(std::chrono::seconds(1));
+                std::cout << "world " << i << std::endl;
+                return i * i;
+                })
+        );
+    }
+
+    for (auto&& result : results)
+        std::cout << result.get() << ' ';
+    std::cout << std::endl;
+
+    return 0;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
